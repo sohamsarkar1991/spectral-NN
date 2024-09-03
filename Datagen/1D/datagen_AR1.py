9be36c31232f16512bfbc5104e70f71e9ed3cdd8
sohamsarkar1991 <93770345+sohamsarkar1991@users.noreply.github.com>
2024-09-03 13:36:46 +0530

    added codes for simple AR(1) model

---
+++ Datagen/1D/datagen_AR1.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#############################################################
+##### Codes for generation of 1D functional time series #####
+############ To be used in the case of d=1 only #############
+#############################################################
+
+
+import numpy as np
+from scipy import special
+import itertools
+
+
+def locations(K,d):
+    gr = np.arange(1,K+1)/(K+1)
+    grid = list(itertools.product(gr,repeat=d))
+    grid = np.asarray(grid)
+    return grid
+
+def locations_unif(M,d):
+    loc = np.random.uniform(low=0.,high=1.,size=[M,d])
+    return loc
+
+def pixelate_single(s,K):
+    for j in np.arange(1,K):
+        if s<(j+0.5)/(K+1):
+            return j-1
+    return K-1
+
+def pixelate(u,K):
+    idx = []
+    for s in u:
+        idx.append(pixelate_single(s,K))
+    return idx
+
+def indices(u,K,d):
+    idx = np.apply_along_axis(pixelate,1,u,K)
+    c = np.zeros(idx.shape[0],dtype=int)
+    for i in range(d):
+        c += idx[:,i]*K**(d-1-i)
+    return c
+
+def print_indices(u,v,K,d,file='indices.dat'):
+    idx1 = indices(u,K,d)
+    idx2 = indices(v,K,d)
+    idx = np.vstack((idx1,idx2))
+    np.savetxt(file,idx,fmt='%d')
+
+def print_locations(K,d,file = 'locations.dat'):
+    u = locations(K,d)
+    np.savetxt(file,u,fmt='%.10f')
+    
+def bessel(s,t,nu):
+    r = abs(s-t)
+    c = 2**nu * special.gamma(nu+1) * r**(-nu) * special.jv(nu,r)
+    return c
+
+def BM(s,t):
+    if s<=t and s>0.:
+        return s
+    elif s>t and t>0.:
+        return t
+    elif s<0. and t<0.:
+        return BM(-s,-t)
+    else:
+        return 0.
+
+def fBM(s,t,alpha):
+    c = 0.5 * (abs(t)**(2*alpha) + abs(s)**(2*alpha) - abs(t-s)**(2*alpha))
+    return c
+
+def fBM2(s,t,H):
+    alpha = 2*H
+    if s<=t and s>0.:
+        return 0.5*(t**alpha + s**alpha - (t-s)**alpha)
+    elif s>t and t>0.:
+        return 0.5*(t**alpha + s**alpha - (s-t)**alpha)
+    elif s<0. and t<0.:
+        return fBM2(-s,-t,H)
+    else:
+        return 0.
+
+def iBM(s,t):
+    if s<=t and s>0.:
+        return s**2*t/2 - s**3/6
+    elif s>t and t>0.:
+        return t**2*s/2 - t**3/6
+    elif s<0. and t<0.:
+        return iBM(-s,-t)
+    else:
+        return 0.
+
+def Bbridge(s,t,T=1.):
+    if s<=t and s>0.:
+        return s*(T-t)/T
+    elif s>t and t>0.:
+        return t*(T-s)/T
+    elif s<0. and t<0.:
+        return Bbridge(-s,-t,T)
+    else:
+        return 0.
+
+def cauchy(s,t,gamma):
+    r = abs(s-t)
+    return 1./(1+r**2)**gamma
+
+def dampedcos(s,t,lam):
+    r = abs(s-t)
+    c = np.exp(-lam*r) * np.cos(r)
+    return c
+
+def fractgauss(s,t,alpha):
+    r = abs(s-t)
+    c = 0.5*((r+1)**alpha + abs(r-1)**alpha - 2*r**alpha)
+    return c
+
+def matern(s,t,nu,rho=1.):
+    r = abs(s-t)/rho
+    if r==0:
+        r = np.finfo(float).eps
+    if nu == 0.5:
+        c = np.exp(-r)
+    elif nu == 1.5:
+        c = r * np.sqrt(3)
+        c = (1.+c) * np.exp(-c)
+    elif nu == 2.5:
+        c = r*np.sqrt(5)
+        c = (1.+c+c**2/3.) * np.exp(-c)
+    elif nu == np.inf:
+        c = np.exp(-r**2/2.)
+    else:  # general case; expensive to evaluate
+        tmp = np.sqrt(2*nu) * r
+        c = 2**(1.-nu) / special.gamma(nu)
+        c *= tmp ** nu
+        c *= special.kv(nu,tmp)
+    return c
+
+def cov_mat(K,d,method,O=None):
+    u = locations(K,d)
+    if O is not None:
+        u = np.matmul(u,O.T)
+    D = int(K**d)
+    C = np.zeros([D,D],dtype=float)
+    for i in range(D):
+        s = u[i,:]
+        for j in range(i,D):
+            t = u_[j]
+            C[i,j] = method(s,t)
+            C[j,i] = C[i,j]
+    return C
+
+def cross_cov(u,v,method,O=None):
+    if u.shape != v.shape:
+        print('Mismatch in shapes of u and v!')
+        return
+    M = u.shape[0]
+    if O is not None:
+        u = np.matmul(u,O.T)
+        v = np.matmul(v,O.T)
+    C = np.zeros(M,dtype=float)
+    for i in range(M):
+        s = u[i]
+        t = v[i]
+        C[i] = method(s,t)
+    return C
+
+def sqrt_mat(C):
+    lam, E = np.linalg.eigh(C)
+    idx = lam>0.
+    lam = np.sqrt(lam[idx])
+    E = E[:,idx]
+    return [lam,E]
+
+
+def datagen_AR1_simple(N,K,d,replicates=1,method=None,rot=None,gam=0.5,N0=100):
+    """
+    Data generation from superpositions of separable processes.
+    INPUT:
+        N - number of observations to be generated
+        K - grid size in 1D
+        d - dimension
+        replicates - number of replications
+        method - process for each separable component
+        rot - rotation matrix, to be applied
+        N0 - burn-in sample: number of samples to be discarded for stationarity
+    OUTPUT:
+        Generates a simple AR(1) process X_t = gam * X_{t-1} + Z_t on a regular grid.
+        Writes the locations of the grid points on the file 'locations.dat'. 
+        Generates N curves at the grid points and writes them on
+        'Examplex.dat', for x=1,...,replicates.
+    """
+    ##### For the innovation Z #####
+    ##### Z and X_0 have the same distribution
+    print('Basis generation started')
+    lam, E = sqrt_mat(cov_mat(K,d,method,rot))
+    print('Basis generated')
+    
+    for repl in range(replicates):
+        #np.random.seed(int(np.random.rand()*(2**32-1)))
+        print('Replicate '+str(repl+1))
+        filename = 'locations'+str(repl+1)+'.dat'
+        print_locations(K,d,filename)
+        filename = 'Example'+str(repl+1)+'.dat'
+        f=open(filename,'w')
+        f.close()
+        x0 = np.random.normal(loc=0.,scale=1,size=len(lam))
+        x0 = np.sum(E*lam*x,axis=1).reshape(1,-1)
+        for n in range(N0): #burn-in, not to be saved
+            z = np.random.normal(loc=0.,scale=1.,size=len(lam))
+            z = np.sum(E*lam*x,axis=1).reshape(1,-1)
+            x0 = gam*x0 + z
+        f = open(filename,'a')
+        for n in range(N): #after burn-in, to be saved
+            z = np.random.normal(loc=0.,scale=1.,size=len(lam))
+            z = np.sum(E*lam*x,axis=1).reshape(1,-1)
+            x0 = gam*x0 + z
+            np.savetxt(f,x0,fmt='%.10f')
+        f.close()
+
